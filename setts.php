8ea7d356a129643512033f058c711ae9f366e0d8
KarlosFanta <leeuwenpatrol@gmail.com>
2014-10-15 21:45:47 +0200

    Create setts.php

---
+++ setts.php
@@ -0,0 +1,21 @@
+
+<?php
+include "header.php";
+include "Installation.html";
+?>
+//create the databse:
+CREATE DATABASE myDB;
+//SHOW DATABASES;
+//activate the mysql databse:
+USE myDB;
+
+CREATE TABLE customer
+(CustNo integer not null  AUTO_INCREMENT  primary key, 
+CustFN VARCHAR(140),
+CustLN VARCHAR(140),
+ABBR varchar(10),
+);
+<?php
+include "setts2.php";
+?>
+
