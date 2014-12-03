3c5c9974a61580ded948734c734e6a1e7480175b
KarlosFanta <leeuwenpatrol@gmail.com>
2014-12-03 13:09:49 +0200

    Create company.php

---
+++ company.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+</head>
+
+<body>
+
+<?php	$page_title = "Company Details";
+	require_once('header.php');	
+?>
+
+
+My BBM: 3333333<br />
+<br />
+NB: For cash payments please make sure you receive a signed receipt.<br />
+<br />
+
+<br />
+Thank you<br />
+CompanyName<br />
+CompanyLogo<br />
+CompanyLogo2<br />
+<br />
+VAT no CopmanyVATnumber <br />
+CompanyWebsite<br />
+Skype: CompanySkype<br />
+Email:CompanyEMail<br />
+Cell: CompanyCell<br />
+Tel: CompanyTel<br />
+Fax: CompanyFax<br />
+<br />
+TermsWebPage<br />
+SupportWebpage<br />
+<br />
+<br />
+
