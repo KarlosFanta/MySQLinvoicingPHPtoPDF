d1025311734c19bfbac7b8038c62b77dd385e4d2
KarlosFanta <leeuwenpatrol@gmail.com>
2014-12-03 13:03:30 +0200

    Create addTrans2sess.php

---
+++ addTrans2sess.php
@@ -0,0 +1,26 @@
+
+
+<?php
+@session_start();
+require_once('header.php');	
+$_SESSION['sel'] = "add_trans2";
+
+	if (@$_SESSION['CustNo'] == "")  //works if session was destroyed
+	{
+	//echo "no session<br />";
+	require_once('selectCust.php');	
+	}
+	else
+	//echo "SESSION CustNo: ". $_SESSION['CustNo'] ."<br />";
+	echo 'addTransCustProcess2sess.php';	
+	include('addTransCustProcess2sess.php');	
+	
+	//    addTransCustProcess2sess.php
+	
+	//require_once('view_cust.php');	
+
+	
+	
+?>
+
+
