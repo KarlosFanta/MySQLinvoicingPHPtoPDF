0a0f215491279e33c2cda884e087319aad7816d3
KarlosFanta <leeuwenpatrol@gmail.com>
2014-12-03 11:14:37 +0200

    Create search2.php

---
+++ search2.php
@@ -0,0 +1,32 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'Itsmeagain007#');
+define('DB_NAME', 'kc');
+
+ 
+if (isset($_GET['term'])){
+    $return_arr = array();
+ 
+    try {
+        $conn = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+       // $stmt = $conn->prepare('SELECT summary FROM ajaxsuggest WHERE summary LIKE :term');
+        $stmt = $conn->prepare('SELECT summary FROM ajaxsuggest');
+        $stmt->execute(array('term' => '%'.$_GET['term'].'%'));
+        
+        while($row = $stmt->fetch()) {
+            $return_arr[] =  $row['summary'];
+        }
+ 
+    } catch(PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }
+ 
+ 
+    /* Toss back results as json encoded array. */
+    echo json_encode($return_arr);
+}
+ 
+?>
