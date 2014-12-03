0556cb99a90a9d00eb0c0e42ce7517796407ba8a
KarlosFanta <leeuwenpatrol@gmail.com>
2014-12-03 13:32:57 +0200

    Create del_trans_process_last.php

---
+++ del_trans_process_last.php
@@ -0,0 +1,149 @@
+<?php	//this is "process_Trans.php"
+ $page_title = "You added a transaction";
+	include('header.php');	
+//oracle: $conn = oci_connect("system", "1234", "localhost/XE");
+require_once('inc_OnlineStoreDB.php');//mysql connection and database selection
+?>
+
+
+
+<?php
+$Trans_No = 0;
+$CustNo = '';
+$TransDate = '';
+$AmtPaid = '';
+$Notes ='';
+$CustSDR ='';
+$TMethod = '';
+$InvNoA = 0;
+$InvNoAincl = 0;
+$InvNoB = 0;
+$InvNoBincl = 0;
+$InvNoC = 0;
+$InvNoCincl = 0;
+$InvNoD = 0;
+$InvNoDincl = 0;
+$InvNoE = 0;
+$InvNoEincl = 0;
+$InvNoF = 0;
+$InvNoFincl = 0;
+$InvNoG = 0;
+$InvNoGincl = 0;
+$InvNoH = 0;
+$InvNoHincl = 0;
+
+$Trans_No = $_POST['TransNo'];
+$CustNo = $_POST['CustNo'];
+$TransDate = $_POST['TransDate'];
+$AmtPaid = $_POST['AmtPaid'];
+$Notes = $_POST['Notes'];
+$CustSDR = $_POST['CustSDR'];
+$TMethod = $_POST['TMethod'];
+$InvNoA = $_POST['InvNoA'];
+$InvNoAincl = $_POST['InvNoAincl'];
+$InvNoB = $_POST['InvNoB'];
+$InvNoBincl = $_POST['InvNoBincl'];
+$InvNoC = $_POST['InvNoC'];
+$InvNoCincl = $_POST['InvNoCincl'];
+$InvNoD = $_POST['InvNoD'];
+$InvNoDincl = $_POST['InvNoDincl'];
+$InvNoE = $_POST['InvNoE'];
+$InvNoEincl = $_POST['InvNoEincl'];
+$InvNoF = $_POST['InvNoF'];
+$InvNoFincl = $_POST['InvNoFincl'];
+$InvNoG = $_POST['InvNoG'];
+$InvNoGincl = $_POST['InvNoGincl'];
+$InvNoH = $_POST['InvNoH'];
+$InvNoHincl = $_POST['InvNoHincl'];
+$Priority = $_POST['Priority'];
+
+
+
+
+echo "Thank you for deleting the transaction: ".$Trans_No." ".$CustNo ." ".$TransDate ."."  ;
+
+$Trans_NoInt = intval($Trans_No);
+$query="delete from transaction where TransNo = $Trans_NoInt";
+//oracle: $query="update transaction set Transfn = '$CustNo', Transln ='$TransDate', Transtel = '$AmtPaid', Transcell= '$Notes', Transemail = '$TMethod', Transaddr = '$InvNoA', distance = '$InvNoAincl' where Transno = :Trans_NoInt";
+echo '</br></br></br></br></br></br></br>';
+
+mysqli_query($DBConnect, $query);
+printf("Affected rows (UPDATE): %d\n", mysqli_affected_rows($DBConnect));
+echo $query;
+
+echo ";<br>";
+$file = "FileWriting/bkp.php";
+include("FileWriting/FileWriting.php");
+//$open = fopen($file, "a+"); //open the file, (e.g.log.htm).
+//fwrite($open, "<br><br><b>Register:</b> " .$query . "<br/>"); 
+//fwrite($open, "<b>Date & Time:</b>". date("d/m/Y"). "<br/>"); //print / write the date and time they viewed the log.
+//fclose($open); // you must ALWAYS close the opened file once you have finished.
+//echo "<br /><br />Check log file: <a href = '.$file.'><br />";
+
+echo "<a href = 'view_trans_all.php'>view_trans_all.php</a></a><br>";
+
+
+
+echo '</br>';echo '</br>';
+//php to sql does not understand semicolon. remove the semicolon!!!
+$TransInt = intval($Trans_NoInt);
+$SQLString = "SELECT * FROM transaction WHERE TransNo = $TransInt";
+//$SQLString = "SELECT * FROM transaction WHERE WHERE CustNo = $item2;
+?>
+<b><font size = "4" type="arial">Edit transaction</b></font>
+</br>
+</br>
+<?php
+if ($result = mysqli_query($DBConnect, $SQLString)) {
+  while ($row = mysqli_fetch_assoc($result)) {
+$item1 = $row["TransNo"];
+$item2 =  $row["CustNo"];
+$CustInt =  $row["CustNo"];
+$item3 = $row["TransDate"];
+$item4 = $row["AmtPaid"];
+$item5 = $row["Notes"];
+$item6 = $row["CustSDR"];
+$item7 = $row["TMethod"];
+$item8 = $row["InvNoA"];
+$item9 = $row["InvNoAincl"];
+$item10 = $row["Priority"];
+print "$item1";
+print "_".$item2;
+print "_".$item3;
+print "_R".$item4;
+print "_".$item5;
+print "_".$item6;
+print "_".$item7;
+print "_".$item8;
+print "_".$item9;
+print "_".$item10;
+}
+$result->free();
+}	echo "<br>";
+//$mysqli->close();
+?>
+
+
+<!--
+//$query="insert into transaction values(5, 'Jn', 'VM', '65', '084', 'johnATv', 'USA', 55)";
+//php to sql does not understand semicolon. remove the semicolon!!!
+//$stmt = oci_parse($conn,$query);
+
+//$rc=oci_execute($stmt);-->
+<?php
+/*
+//oracle: 
+if(!$rc)
+{
+$e=oci_error($stmt);
+var_dump($e);
+}
+
+oci_commit($conn);
+
+oci_free_statement($stmt);
+oci_close($conn);
+//}
+*/
+?>
+
