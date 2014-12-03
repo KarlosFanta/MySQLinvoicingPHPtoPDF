769af62afc287b058f5d65b19174a0a5c1cf90d9
KarlosFanta <leeuwenpatrol@gmail.com>
2014-12-03 11:18:37 +0200

    Create PDF.php

---
+++ PDF.php
@@ -0,0 +1,30 @@
+<?php
+//============================================================+
+// File name   : example_048.php
+// Begin       : 2009-03-20
+// Last Update : 2013-05-14
+//
+// Description : Example 048 for TCPDF class
+//               HTML tables and table headers
+//
+// Author: Nicola Asuni
+//
+// (c) Copyright:
+//               Nicola Asuni
+//               Tecnick.com LTD
+//               www.tecnick.com
+//               info@tecnick.com
+//============================================================+
+
+/**
+ * Creates an example PDF TEST document using TCPDF
+ * @package com.tecnick.tcpdf
+ * @abstract TCPDF - Example: HTML tables and table headers
+ * @author Nicola Asuni
+ * @since 2009-03-20
+ */
+
+// Include the main TCPDF library (search for installation path).
+include('PDF/tcpdf/examples/PDF.php');
+
+?>
