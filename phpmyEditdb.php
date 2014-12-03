29381096ad3e287ecfb7e7a53005293790fe94f3
KarlosFanta <leeuwenpatrol@gmail.com>
2014-12-03 11:17:49 +0200

    Create phpmyEditdb.php

---
+++ phpmyEditdb.php
@@ -0,0 +1,19 @@
+
+
+<?php
+$opts['hn'] = 'localhost';
+$opts['un'] = 'root'; //change to root or progress_root
+$opts['pw'] = 'Itsmeagain007#';
+$opts['db'] = 'kc'; //chenge to kc or progress_kc
+
+//if they are wrong they show phpMyEdit error: could not connect to SQL on my localhost webserver.
+
+//http://www.progressive-energy.co.za/phpMyAdmin-3.5.2-english/
+//NOT THE TABLE HERE!!!
+//$opts['tb'] = 'customer';
+
+
+//  $DBConnect = new mysqli("localhost", "root","Itsmeagain007#", "kc");//error control operator @ suppresses the error messages TEST Q
+// $DBConnect = new mysqli("localhost", "progress_root","Itsmeagain007#", "progress_kc");//error control operator @ suppresses the error messages TEST Q
+
+?>
