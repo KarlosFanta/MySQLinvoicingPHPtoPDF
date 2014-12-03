d1946cb6d8aeaa6e4bdc7a6c83806d5d38a31b45
KarlosFanta <leeuwenpatrol@gmail.com>
2014-12-03 11:47:59 +0200

    Create ERD.php

---
+++ ERD.php
@@ -0,0 +1,23 @@
+<?php
+
+
+	
+	require_once('header.php');	
+			
+
+?> 
+<?php //require_once "header.php"; ?>
+<b><font size = "4" type="arial">ERD</b></font>
+</br>
+</br>
+</br><img src = "ERD.bmp"/>
+</br>ERD.bmp
+</br>
+
+
+
+
+<?php
+	print "";
+	require_once('footer.php');		
+?>
