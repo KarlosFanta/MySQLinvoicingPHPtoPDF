1866dd53673508708e83d0e3f5f58caac7c04106
KarlosFanta <leeuwenpatrol@gmail.com>
2014-12-03 10:45:14 +0200

    Create signaturePaid.php

---
+++ signaturePaid.php
@@ -0,0 +1,26 @@
+<BR />
+Hi.<BR />
+<BR />
+
+Please view attached PAID invoice.<BR />
+The attached invoice has been already paid for.<br>
+You have already paid for this invoice.<br>
+<BR />There is no need to call me about this invoice.
+<BR />
+<BR />
+Thank you<BR />
+CompanyName<BR />
+CompanyLogo<BR />
+CompanyLogo<BR />
+CompanyWebsite<BR />
+CompanyContact<BR />
+Fax: CompanyFax<BR />
+Skype: SkypeName<BR />
+<BR />
+Sales terms: <a href= "http://CompanyTermsSite">CompanyTermsSite</a><BR />
+
+<BR />
+<BR />
+Support: <a href ="http://CompanySupportSite">CompanyTermsSite</a><BR />
+<BR />
+
