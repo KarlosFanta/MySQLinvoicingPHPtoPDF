105684767573b7929f0c88fa5767dace7bc2228e
KarlosFanta <leeuwenpatrol@gmail.com>
2014-12-03 13:09:37 +0200

    Create comment.php

---
+++ comment.php
@@ -0,0 +1,53 @@
+<?php
+	require_once("inc_OnlineStoreDB.php");
+	$queryS = "SELECT * FROM comment ORDER BY id DESC LIMIT 1";
+
+	if ($result2 = mysqli_query($DBConnect, $queryS)) {
+	  while ($row2 = mysqli_fetch_assoc($result2)) {
+			$item1b = $row2["message"];
+			$row_cnt = mysqli_num_rows($result2);
+		}
+	$result2->free();
+	}
+?>
+
+<html>
+   <head>
+ 
+<!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>-->
+<script src="jquery-1.10.1.min.js"></script>
+        
+       <script type="text/javascript">
+               $(document).ready(function(){
+                    $("#button").click(function(){
+ 
+                          var name=$("#name").val();
+                          var message=$("#message").val();
+ 
+                          $.ajax({
+                              type:"post",
+                              url:"process2.php",
+                              data:"name="+name+"&message="+message,
+                              success:function(data){
+                                 $("#info").html(data);
+                              }
+ 
+                          });
+ 
+                    });
+               });
+			   
+				   
+       </script>
+   </head>
+ 
+ <body>
+   <form name="daBigNote">
+	<?php $rowsE = substr_count( $item1b, "\n" )+2;  ?>
+	<textarea name="message" id="message" rows="<?php echo $rowsE; ?>" cols="70" ><?php echo $item1b; ?></textarea>
+	<br/>
+	<input type="button" value="Update Notes" id="button">
+	<div id="info" />
+   </form>
+  </body>
+</html>
