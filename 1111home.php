7c6efdf801067f3bfee9d1b80adc8d35c1815186
KarlosFanta <leeuwenpatrol@gmail.com>
2014-12-03 12:46:56 +0200

    Create 1111home.php

---
+++ 1111home.php
@@ -0,0 +1,63 @@
+
+<?php	$page_title = "Add a Customer";
+	require_once('header.php');	
+//$conn = oci_connect("system", "1234", "localhost/XE");
+//C:\oraclexe\app\oracle\product\11.2.0\server\bin\StartDB.bat
+?>
+
+
+
+
+<HTML>
+<TITLE>Enter page title here </TITLE>
+<HEAD>
+</HEAD>
+
+<body>
+<BODY>
+<BODY BGCOLOR="#FFFFFF">
+<FONT FACE="arial" SIZE=5>
+Welcome to the PHP Accounting and Inventory Management System</br></font>
+</br>
+</br>
+Here you can manage quotes, orders and invoices.</br>
+</br>
+<?php 
+	//require_once('header.php');	
+	echo "Today's date: ";
+	echo date("l F d, Y"); 
+	print "</br></br>PHP</br></br>";
+	
+	echo "intval(4.8): ";
+	echo intval(-4.8); 
+	print "</br></br></br></br>";
+
+	echo "round(4.8): ";
+	echo -(round(-4.8)); 
+	print "</br></br></br></br>";
+
+
+	echo "round(4.8, 0, PHP_ROUND_HALF_UP): ";
+	echo round(-4.8, 0, PHP_ROUND_HALF_UP);   
+	print "</br></br>";
+
+?>
+<br />
+
+<br />
+<br />
+
+
+
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
