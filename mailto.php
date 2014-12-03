87d9d79258160cabcd02325a5d206e3b62e0dc9b
KarlosFanta <leeuwenpatrol@gmail.com>
2014-12-03 11:19:52 +0200

    Create mailto.php

---
+++ mailto.php
@@ -0,0 +1,12 @@
+
+
+<script src="http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+ 
+function redirect()
+{
+    location.href = "mailto:mail@example.org?subject=yo";
+}
+</script>
+
+<body onload="javascript: redirect();">
