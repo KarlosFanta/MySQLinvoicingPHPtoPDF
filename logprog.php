b3a79629abeaabfa6c4763f755060a2928dab98d
KarlosFanta <leeuwenpatrol@gmail.com>
2014-12-03 11:20:19 +0200

    Create logprog.php

---
+++ logprog.php
@@ -0,0 +1,12 @@
+
+<?php
+require_once ("header.php");
+
+	$date = date("H:i dS F"); //Get the date and time.
+	echo $date;
+	$file = "log.php"; //Where the log will be saved.
+
+	
+?>
+<body>
+</html>
