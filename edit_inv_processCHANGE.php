cf7d8770960f57373fab795f0be68004c52951f6
KarlosFanta <leeuwenpatrol@gmail.com>
2014-12-03 13:37:45 +0200

    Create edit_inv_processCHANGE.php

---
+++ edit_inv_processCHANGE.php
@@ -0,0 +1,16 @@
+<?php	//this is "edit_inv_process.php"
+	require_once('header.php');	
+	require_once ('inc_OnlineStoreDB.php');
+
+//!! This is an inbetween form which launches the process_last.php form!!!
+//!!here we view the details of the invoice before we change him on the last form.
+$TBLrow = $_POST['mydropdownEC'];
+$Invno = explode(';', $TBLrow );
+$InvNo = intval($Invno[0]);
+require_once "view_inv_one.php";
+include "edit_inv_processECHANGE.php";
+//include ("view_inv_prev_by_cust.php");
+//include ("view_trans_by_cust.php");
+?> 
+
+view ALL invoices of ALL customers: <a href=view_inv.php>view_inv.php</a>
