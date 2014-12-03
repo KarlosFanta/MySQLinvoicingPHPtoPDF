93a391e2272391308430640b3723b670de687bf5
KarlosFanta <leeuwenpatrol@gmail.com>
2014-12-03 10:45:25 +0200

    Create signature.php

---
+++ signature.php
@@ -0,0 +1,48 @@
+<BR />
+Hi.<BR />
+<BR />
+Thank you for your order.<BR />
+Please view attached invoice and kindly arrange for payment.<BR />
+(More invoices following)<br>
+A statement can be requested.<br>
+<BR />
+Recommended payment reference: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <?php echo  $SDR;  ?><BR />
+Recommended statement description (DR): <?php echo  $SDR;  ?><BR />
+Beneficiary statement description (CR): &nbsp;&nbsp;<?php echo  $SDR;  ?><BR />
+<BR />
+Please send proof of payments to C<BR />
+<BR />
+<BR />
+Banking details:<BR /><BR />
+Account holder: CompanyName<br />
+Bank: <br />
+Branch Code:  (Universal branch number)<br />
+(Other branch codes:   )<br />
+Account Number: <br />
+(Branch: )<br />
+ Type of Account: <br />
+<br /><FONT FACE="Arial, sans-serif" size = "2">
+<!--Please send proof of payment to: CompanyEmail@me.co.za<br /><BR />-->
+NB: For cash payments please make sure you have a receipt with a signature.<BR />
+
+<BR />
+<BR />
+VAT no:  <BR />
+<BR />
+Thank you<BR />
+CompanyName<BR />
+Logo<BR />
+CompanyLogo2<BR />
+CompanyWebsite<BR />
+COntactNos<BR />
+Fax: FaxNo<BR />
+Skype: SkypeName<BR />
+<BR />
+Sales terms: <a href= "http://www.TermsPage">TermsPage</a><BR />
+
+<BR />
+Payment options:  EFT, Cheque to CompanyName or direct deposit or cash by hand to ComanyAddress.<BR />
+<!--NB: For cash payments please make sure you have a receipt with my signature.<BR />-->
+<BR />
+Internet & Email Support: <a href ="http://SupportPage">SupportPage</a><BR />
+<BR />
