ebf6d3819c91a1db8f01629b4c16585e51a88b3d
KarlosFanta <leeuwenpatrol@gmail.com>
2014-12-10 09:25:00 +0200

    Create CalcServ.php

---
+++ CALC/CalcServ.php
@@ -0,0 +1,35 @@
+<?php
+
+if ($_REQUEST["manipulator"] == "multiply")
+  {
+    $value3 = $_REQUEST["value1"] * $_REQUEST["value2"];
+    $sign = " x ";
+    $eq = " = ";
+  }
+
+elseif ($_REQUEST["manipulator"] == "divide")
+  {
+    $value3 = $_REQUEST["value1"] / $_REQUEST["value2"];
+    $sign = " / ";
+    $eq = " = ";
+  }
+  
+elseif ($_REQUEST["manipulator"] == "subtract")
+  {
+    $value3 = $_REQUEST["value1"] - $_REQUEST["value2"];
+    $sign = " - ";
+    $eq = " = ";
+  }  
+  
+elseif ($_REQUEST["manipulator"] == "add")
+  {
+    $value3 = $_REQUEST["value1"] + $_REQUEST["value2"];
+    $sign = " + ";
+    $eq = " = ";
+  }  
+  
+  $response = $_REQUEST["value1"] . $sign . $_REQUEST["value2"] . $eq . $value3;
+  
+echo $response;
+
+?>
