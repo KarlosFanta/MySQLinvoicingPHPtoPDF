5409ec56ca5ee76f0f797be055d72ae9df5a78c3
KarlosFanta <leeuwenpatrol@gmail.com>
2018-02-11 21:11:26 +0200

    Add files via upload

--- veExp.php
+++ veExp.php
@@ -12,26 +12,39 @@
 
 <form name="Addcust" action="add_CustProcess.php" onsubmit="return validateForm()" method="post">
 		<div>
+<h1> View All Expenses</h1>
+<a href="./viewExp.php">View All Expenses</a></br>
+<a href = 'viewExpHEandExp.php'>viewExpHEandExp</a></br>
+<a href = 'viewExpmyedit.php'>viewExpmyedit</a></br>
+<a href = 'viewExpmyeditbasic.php'>viewExpmyeditbasic for editing</a></br>
+<a href = 'viewExpHmyeditbasic.php'>viewExpHmyeditbasic for editing</a></br>
+<a href = 'viewExpEmyeditbasic.php'>viewExpEmyeditbasic for editing</a></br>
+<a href = 'viewExpALLmyeditbasic.php'>viewExpALLmyeditbasic for editing</a></br>
+<a href="./viewExpAll.php">View All Expenses</a></br></br>
 <h1> QuickEdit ExpensesC</h1>
 
-<a href = "editExpQ.php">QuickEdit Expenses</a></br></br></br>
-<a href = "editExpCQ.php">QuickEdit ExpensesC of selected customer</a></br></br></br>
+<a href = "editExpQ.php">QuickEdit Expenses</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href = "editExpH.php">QuickEdit HomeExpenses (Cyb, Outsur, SARS, Eskom, Tel, BankFees, CellC,Food,Private)</a><br><a href = "editExpE.php">QuickEdit ExtraExpenses(Accountant, petrol, Carrepairs, Medical)</a></br></br>
+<a href = "editExpCQ.php">QuickEdit ExpensesC of selected customer</a></br></br>
+<a href = "moveExp.php">Move an expense from one table to another</a></br></br>
 
 
 <h1> Edit Expenses</h1>
-<a href = "editExp.php">Edit Any Expenses </a></br></br></br>
+<a href = "editExp.php"></a>Edit Any Expenses </br></br>
+<!--<a href = 'selectCustAssignStk.php'>Assign Stock to any Customer</a></br>-->
+<a href = 'selectCustAssignStkInv.php'>Assign customer's Stock to Customer's invoice</a> &nbsp;&nbsp;
+assignStkInv</br></br>
+<a href = 'selectCustAssignStk.php'>Assign any Stock to a Customer and then invoice</a></br></br>
 
 <h1> View Expenses of Selected Customer</h1>
-<a href="./viewExpCust.php">Customer Expenses</a></br></br></br>
-<a href="./viewExpCustALL.php">Customer All Expenses and all Invoices</a></br></br></br>
+<a href="./viewExpCust.php">Customer Expenses</a></br>
+<a href="./viewExpCustS.php">Select Customer, then show his/her Expenses</a></br></br>
+<a href="./viewExpCustALL.php">Customer's All Expenses and all Invoices</a></br></br></br>
 
 
-<h1> View All Expenses of ALL Customers</h1>
-<a href="./viewExp.php">View All Expenses</a></br></br></br>
 
-<h1> Edit All Expenses</h1>
-<a href="./viewExpAll.php">Edit All Expenses</a></br></br></br>
 
+<h1> Check Expenses of a supplier</h1>
+<a href = "editExp2.php">Edit Any Expenses of certain supplier </a></br></br></br>
 
 
 
