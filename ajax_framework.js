ba9c81fbe155826b8f22ab494825bfca39f679de
Caleb Mazalevskis <maikuolan@gmail.com>
2020-11-11 19:17:09 +0800

    Clean up JavaScript.

--- ajax_framework.js
+++ ajax_framework.js
@@ -1,42 +1,43 @@
 /* ---------------------------- */
-/* XMLHTTPRequest Enable */
+/* XMLHTTPRequest Enable        */
 /* ---------------------------- */
 function createObject() {
-var request_type;
-var browser = navigator.appName;
-if(browser == "Microsoft Internet Explorer"){
-request_type = new ActiveXObject("Microsoft.XMLHTTP");
-}else{
-request_type = new XMLHttpRequest();
-}
-return request_type;
+    var request_type;
+    var browser = navigator.appName;
+    if (browser == "Microsoft Internet Explorer") {
+        request_type = new ActiveXObject("Microsoft.XMLHTTP");
+    } else {
+        request_type = new XMLHttpRequest();
+    }
+    return request_type;
 }
 
 var http = createObject();
 
-
 /* -------------------------- */
-/* INSERT */
+/* INSERT                     */
 /* -------------------------- */
 /* Required: var nocache is a random number to add to request. This value solve an Internet Explorer cache issue */
 var nocache = 0;
+
 function insert() {
-// Optional: Show a waiting message in the layer with ID login_response
-document.getElementById('insert_response').innerHTML = "Just a second..."
-// Required: verify that all fileds is not empty. Use encodeURI() to solve some issues about character encoding.
-var site_url= encodeURI(document.getElementById('site_url').value);
-var site_name = encodeURI(document.getElementById('site_name').value);
-// Set te random number to add to URL request
-nocache = Math.random();
-// Pass the login variables like URL variable
-http.open('get', 'insert.php?site_url='+site_url+'&site_name=' +site_name+'&nocache = '+nocache);
-http.onreadystatechange = insertReply;
-http.send(null);
+    // Optional: Show a waiting message in the layer with ID login_response
+    document.getElementById('insert_response').innerHTML = "Just a second..."
+    // Required: verify that all fileds is not empty. Use encodeURI() to solve some issues about character encoding.
+    var site_url = encodeURI(document.getElementById('site_url').value);
+    var site_name = encodeURI(document.getElementById('site_name').value);
+    // Set the random number to add to URL request
+    nocache = Math.random();
+    // Pass the login variables like URL variables
+    http.open('get', 'insert.php?site_url=' + site_url + '&site_name=' + site_name + '&nocache = ' + nocache);
+    http.onreadystatechange = insertReply;
+    http.send(null);
 }
+
 function insertReply() {
-if(http.readyState == 4){
-var response = http.responseText;
-// else if login is ok show a message: "Site added+ site URL".
-document.getElementById('insert_response').innerHTML = 'Site added:'+response;
-}
+    if (http.readyState == 4) {
+        var response = http.responseText;
+        // else if login is ok show a message: "Site added+ site URL".
+        document.getElementById('insert_response').innerHTML = 'Site added:' + response;
+    }
 }
