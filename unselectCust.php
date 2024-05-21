b2505d031cc3c811eec9ae0dea0fd941f9eb557e
KarlosFanta <leeuwenpatrol@gmail.com>
2024-05-21 12:14:03 +0200

    Create unselectCust.php

---
+++ unselectCust.php
@@ -0,0 +1,20 @@
+
+<?php
+include('header.php');	 
+
+
+   @session_start();
+echo "SESSION CustNo: ". @$_SESSION['CustNo'] ."<br />";
+$_SESSION = array();
+//$_SESSION['CustNo'] = "1111111111";
+
+session_destroy();
+
+
+//echo "SESSION CustNo: ". $_SESSION['CustNo'] ."<br />";
+echo "Customer unselected. (destroy session)";
+
+
+
+
+?>
